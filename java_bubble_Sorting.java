9e5d166d96c85014818bd3e184061c7ae9516454
ImportTushar <tushargiri9901@gmail.com>
2024-04-26 03:09:53 +0530

    is java_bubble_Sorting

---
+++ java_bubble_Sorting.java
@@ -0,0 +1,28 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class java_bubble_Sorting {
+    public static void BubbleSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j] >arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] =  arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void Printer(int arr[]){
+        for(int i = 0;i <arr.length;i++){
+            System.out.print(arr + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[] = {3,6,2,1,8,7,4,5,3,1};
+        BubbleSort(arr);
+        Printer(arr);
+    }
+}
