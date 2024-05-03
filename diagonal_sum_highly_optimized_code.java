e7f6ffddf53f76aa1271fffa1cc06d7b2a82d672
ImportTushar <tushargiri9901@gmail.com>
2024-05-03 16:57:21 +0530

    is diiagonal Brute fore On^2

--- diagonal_sum_highly_optimized_code.java
+++ diagonal_sum_highly_optimized_code.java
@@ -1,37 +1,26 @@
-public class diagonal_sum_highly_optimized_code{
-        public static void DiagonalSum(int matrix[][]){ // on2
-            int sum = 0;
-            for(int i=0;i<matrix.length;i++){
-                for(int j=0;j<matrix[0].length;j++){
-                    if(i==j){
-                        sum +=matrix[i][j];
-                    }
-                }
-
-                //secondary
-                for(int i=0;i<matrix.length;i++){
-                    for(int j=0;j<matrix.length;j++){
-                        if(i+j == matrix.length-1){
-                             sum += matrix[i][j];
-                        }
-                    }
-                }
-
-            for(int i=0; i<matrix.length; i++){
-                sum += matrix[i][matrix.length-i-1];
-            }
-                
-                return sum;
-            }
-            
+public class diagonal_sum_highly_optimized_code {
+    public static int diagonalSum(int matrix[][]) {
+        int sum = 0;
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            sum += matrix[i][i]; // Primary diagonal
+            sum += matrix[i][n - i - 1]; // Secondary diagonal
         }
+        // Adjust the sum if the middle element is counted twice
+        if (n % 2 == 1) {
+            sum -= matrix[n / 2][n / 2];
+        }
+        return sum;
+    }
 
-        public static void main(String args[]){
-        int matrix[][] = {{1,2,3,4},
-                         {5,6,7,8},
-                         {9,10,11,12},
-                         {13,14,15,16}
-                         };
-        DiagonalSum(matrix);
+    public static void main(String args[]) {
+        int matrix[][] = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        int sum = diagonalSum(matrix);
+        System.out.println("Diagonal sum: " + sum);
     }
 }
